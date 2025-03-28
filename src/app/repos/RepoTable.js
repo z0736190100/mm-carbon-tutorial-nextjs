804fa3ddb902eac74fefe7b4fa12af88919d3489
sburtsev <z0736190100@gmail.com>
2025-03-28 22:58:03 +0200

    step3: fetch from github api add, table update to render dynamic data, pagination add, skeleton loading feature add

--- src/app/repos/RepoTable.js
+++ src/app/repos/RepoTable.js
@@ -16,6 +16,12 @@ import {
 } from '@carbon/react';
 
 const RepoTable = ({ rows, headers }) => {
+
+    const getRowDescription = (rowId) => {
+        const row = rows.find(({ id }) => id === rowId);
+        return row ? row.description : '';
+    };
+
     return (
         <DataTable
             rows={rows}
@@ -50,7 +56,7 @@ const RepoTable = ({ rows, headers }) => {
                                         ))}
                                     </TableExpandRow>
                                     <TableExpandedRow colSpan={headers.length + 1}>
-                                        <p>Row description</p>
+                                        <p>{getRowDescription(row.id)}</p>
                                     </TableExpandedRow>
                                 </React.Fragment>
                             ))}
